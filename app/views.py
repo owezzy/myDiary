3cb26778718c69078e65d24bdfa7487bd480747c
owen_adira <owenobezzy@gmail.com>
2018-08-30 17:26:34 +0300

    db views setup update

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from models import db, EntryModel,EntryModelScheme
 from sqlalchemy.exc import SQLAlchemyError
 
+
 api_bp = Blueprint('api'__name__)
 entry_scheme = EntryModelScheme
 api = Api(api_bp)
@@ -17,29 +18,25 @@ class EntryResource(Resource):
         entry = EntryModel.query.get_or_404(id)
         entry_dict = request.get_json(force=True)
         if 'entry' in entry_dict:
-            message_message = message_dict['message'] 
-            if Message.is_unique(id=id, message=message_message):
-                message.message = message_message
+            entry_entry = entry_dict['entry'] 
+            if EntryModel.is_unique(id=id, entry=entry_entry):
+                entry.entry = entry_entry
             else:
-                response = {'error': 'A message with the same message already exists'}
-                return response, status.HTTP_400_BAD_REQUEST
-        if 'duration' in message_dict:
-            message.duration = message_dict['duration']
-        if 'printed_times' in message_dict:
-            message.printed_times = message_dict['printed_times']
-        if 'printed_once' in message_dict:
-            message.printed_once = message_dict['printed_once']
-        dumped_message, dump_errors = message_schema.dump(message)
+                response = {'error': 'An entry with the same entry already exists'}
+                return response,HTTP_400_BAD_REQUEST
+        if 'title' in entry_dict:
+            entry.title = entry_dict['title']
+        dumped_entry, dump_errors = entry_schema.dump(message)
         if dump_errors:
-            return dump_errors, status.HTTP_400_BAD_REQUEST
-        validate_errors = message_schema.validate(dumped_message)
+            return dump_errors,HTTP_400_BAD_REQUEST
+        validate_errors = entry_schema.validate(dumped_message)
         if validate_errors:
-            return validate_errors, status.HTTP_400_BAD_REQUEST
+            return validate_errors, HTTP_400_BAD_REQUEST
         try:
-            message.update()
+            entry.update()
             return self.get(id)
         except SQLAlchemyError as e:
                 db.session.rollback()
                 resp = {"error": str(e)}
-                return resp, status.HTTP_400_BAD_REQUEST
+                return resp,HTTP_400_BAD_REQUEST
         
